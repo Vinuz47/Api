2a29d1b7246771f6cbe95e4d7b13068faa8d2d9f
Vinura Dulaksith <vinura@azbow.com>
2024-10-22 20:13:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const  user = require('./routers/users');
+const  student = require('./routers/students');
+
+//app.use(log); //middleware usings
+app.use('/api/users',user);  
+app.use('/api/students',student);
+
+//midleware creation
+function log(req,res,next){
+    console.log('middleware');
+    next();
+}
+
+
+console.log(process.env.PORT);
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>console.log(`App is running on port: ${port}`));
