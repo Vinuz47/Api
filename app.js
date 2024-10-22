dac1767c755cec295394115ce64a15539b8fa976
Vinura Dulaksith <vinura@azbow.com>
2024-10-22 22:18:34 +0530

    change

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const app = express();
-const  user = require('./routers/users');
-const  student = require('./routers/students');
+const {MongoClient} = require("mongodb");
+const fs = require('fs');
 
-//app.use(log); //middleware usings
-app.use('/api/users',user);  
-app.use('/api/students',student);
-
-//midleware creation
-function log(req,res,next){
-    console.log('middleware');
-    next();
-}
-
-
-console.log(process.env.PORT);
-const port = process.env.PORT || 3000;
-
-app.listen(port,()=>console.log(`App is running on port: ${port}`));
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const {MongoClient} = require("mongodb");
-const fs = require('fs');
 
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
+const express = require('express');
+const app = express();
+const  user = require('./routers/users');
+const  student = require('./routers/students');
+
+//app.use(log); //middleware usings
+app.use('/api/users',user);  
+app.use('/api/students',student);
+
+//midleware creation
+function log(req,res,next){
+    console.log('middleware');
+    next();
+}
+
+
+console.log(process.env.PORT);
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>console.log(`App is running on port: ${port}`));
+
+module.exports = app;
