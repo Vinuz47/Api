8e4689ac5cfb1431fc81b3c92d5a7e9a9bc4abfb
Vinura Dulaksith <vinura@azbow.com>
2024-10-22 22:42:36 +0530

    donnnn

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,58 @@
 
 const express = require('express');
 const app = express();
-const  user = require('../routers/users');
-const  student = require('../routers/students');
+//const  user = require('../routers/users');
+//const  student = require('../routers/students');
+const {MongoClient} = require("mongodb");
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
 
 //app.use(log); //middleware usings
-app.use('/api/users',user);  
-app.use('/api/students',student);
+// app.use('/api/users',user);  
+// app.use('/api/students',student);
+
+app.get('/usersget',async (req,res)=>{
+    try{
+        // Call findAllDetails to get the data
+        const jsondata = await findAllDetails();
+
+        //send data in JSON format to response
+        res.send(jsondata);
+    }catch(err){
+        console.error(err);
+        res.status(500).send("An error occurred while fetching data.");
+    }
+    
+});
+
+app.post('/userspost',(req,res)=>{
+    res.send({msg:'hello guys user POST'});
+})
+
+async function findAllDetails() {
+    try{
+        const database = client.db('mydb');
+        const collection = database.collection('products');
+
+       
+         // Find all documents in the collection
+         
+         const result = await collection.find().toArray();//Convert the cursor to an array
+
+         // Convert the array of documents to JSON format
+         //const jsonData = await JSON.stringify(result,null,2);// Pretty-print with 2-space indentation
+         
+        // console.log(jsonData);
+
+         return result;
+         
+    }catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.close();
+    }
+}
 
 //midleware creation
 function log(req,res,next){
